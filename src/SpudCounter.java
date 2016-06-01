e0ff57e89e2168695d305f1aa397fe585735ecf4
Lucas Baizer <titaniumsapphire32@gmail.com>
2016-05-31 20:04:08 -0700

    sum
    desc

---
+++ src/SpudCounter.java
@@ -0,0 +1,17 @@
+
+public class SpudCounter {
+	public static void main(String[]args){
+		int spud = 1;
+		
+		for(spud = 1; spud < 10; spud++){
+			System.out.println(spud+" potato");
+		}
+		
+		
+		
+	
+		
+	}
+}
+//Print “1 potato, 2 potato,..... 9 potato”. 
+//[variation: “1 potato, 2 potato, 3 potato, 4, 5 potato, 6 potato, 7 potato, more”] https://youtu.be/efMHLkyb7ho
---
+++ src/TortoiseInSpace.java
@@ -0,0 +1,90 @@
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+
+import org.teachingextensions.logo.Tortoise;
+
+public class TortoiseInSpace implements KeyEventDispatcher {
+int leftInt = 100;
+int rightInt = 100;
+	
+	private void moveTortoise(int keyPressed) {
+	
+System.out.println(keyPressed);
+
+if(keyPressed == 38){
+	Tortoise.move(10);
+}
+
+if(keyPressed == 37){
+	Tortoise.turn(-90);
+	Tortoise.move(10);
+	Tortoise.turn(90);
+}
+if(keyPressed == 39){
+	Tortoise.turn(90);
+	Tortoise.move(10);
+	Tortoise.turn(-90);
+}
+	
+if(keyPressed == 40){
+	Tortoise.turn(180);
+	Tortoise.move(10);
+	Tortoise.turn(180);
+}
+	}
+
+	private void checkIfR2D2Found() throws Exception {
+		int tortoiseLocationX = Tortoise.getX();
+		int tortoiseLocationY = Tortoise.getY();
+
+		if (tortoiseLocationX <= 510 && tortoiseLocationX >= 505 && tortoiseLocationY >= 110 && tortoiseLocationY <= 115)
+			playEureka();
+	}
+
+	public static void main(String[] args) {
+		new TortoiseInSpace().controlTheTortoise();
+	}
+
+	private void controlTheTortoise() {
+		KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(this);
+		Tortoise.getBackgroundWindow().setBackgroundImage(
+				"http://cdn.playbuzz.com/cdn/ae928505-70fe-4538-8fc4-034d1acef441/b7a29f96-7410-440b-9d93-3cfc0abccce9.jpg");
+
+		Tortoise.penUp();
+		Tortoise.setSpeed(10);
+	}
+
+	public boolean dispatchKeyEvent(KeyEvent e) {
+		if (e.getID() == KeyEvent.KEY_PRESSED) {
+			moveTortoise(e.getKeyCode());
+			try {
+				checkIfR2D2Found();
+			} catch (Exception exception) {
+			}
+		}
+		return false;
+	}
+
+	public void playEureka() {
+		System.out.println("EUREKA!");
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(
+					"/Users/League/Google Drive/league-sounds/r2d2-eureka.wav"));
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
+
+
