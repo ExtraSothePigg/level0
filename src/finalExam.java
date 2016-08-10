6572a7ec4927e2e2f285f1d7a592cbf9342365af
LiamHankins <league@WTS5.attlocal.net>
2016-08-09 20:01:00 -0700

    summ
    desc
    
    done

---
+++ src/finalExam.java
@@ -0,0 +1,40 @@
+
+import java.awt.Color;
+import javax.swing.JOptionPane;
+import org.jointheleague.graphical.robot.Robot;
+
+/* Level 0 Exam: Coding  Exercise #1 */
+public class finalExam{
+
+    public static void main(String[] args) {
+   	 // 3. ask the user what color they would like the Robot to draw
+
+   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+
+   	 // 2. set the pen width to 10
+
+   	 // 1. make the Robot draw a shape
+    	
+   	 
+    }
+
+}
+
+
+
+/* Level 0 Exam: Coding  Exercise #2 */
+import javax.swing.JOptionPane;
+
+/**
+ * Write a program that asks the user for their age. 
+ * Tell them which year they were born. 
+ * If they are over 30, tell them they are too old to play this game.
+ **/
+
+public class CodingExercise2 {
+
+}
+
+
+
+
